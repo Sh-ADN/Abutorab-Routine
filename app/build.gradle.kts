d7ea3752b218d61e94c04e18895d01f6cd8a96c5
ADN <ananneyadebnath@gmail.com>
2026-06-12 10:07:26 +0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
 }
 
 android {
-  namespace = "com.example"
+  namespace = "com.abutorab.routine"
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.example"
+applicationId = "com.abutorab.routine"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
